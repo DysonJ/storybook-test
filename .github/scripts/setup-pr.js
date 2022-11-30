625f28e3cdc130e04fb1995ecc5fb76f5de647d6
DysonJ <hello@jondyson.com>
2022-11-29 19:51:05 -0500

    issue

--- .github/scripts/setup-pr.js
+++ .github/scripts/setup-pr.js
@@ -1,8 +1,18 @@
 module.exports = async ({github, context, core}) => {
-  console.log(JSON.stringify(context));
+  //console.log(JSON.stringify(context));
   const creator = context.payload.sender.login;
 
-  await github.request(`PATCH /repos/${context.repo.owner}/${context.repo.repo}/pulls/${context.payload.pull_request.number}`, {body:"hello world"});
+  //await github.request(`PATCH /repos/${context.repo.owner}/${context.repo.repo}/pulls/${context.payload.pull_request.number}`, {body:"hello world"});
+  await github.rest.pulls.update({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.payload.pull_request.number,
+    body: context.payload.pull_request + `/n Another world here`
+  })
+
+  console.log(context.payload.pull_request.number)
+  console.log(context.issue.number)
+
 
   await github.rest.issues.createComment({
     issue_number: context.issue.number,
