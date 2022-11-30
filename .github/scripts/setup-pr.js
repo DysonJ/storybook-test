c5556a32af02a98ade45ca02605893bde27c0f70
DysonJ <hello@jondyson.com>
2022-11-29 20:04:30 -0500

    add assignee

--- .github/scripts/setup-pr.js
+++ .github/scripts/setup-pr.js
@@ -1,23 +1,31 @@
 module.exports = async ({github, context, core}) => {
   //console.log(JSON.stringify(context));
   const creator = context.payload.sender.login;
+  const owner = context.repo.owner
+  const repo = context.repo.repo
+  const issue_number = context.issue.number
 
   //await github.request(`PATCH /repos/${context.repo.owner}/${context.repo.repo}/pulls/${context.payload.pull_request.number}`, {body:"hello world"});
   await github.rest.pulls.update({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    pull_number: context.payload.pull_request.number,
-    body: context.payload.pull_request.body + `\n---\n[DESC-1234](https://pepsico-ecomm.atlassian.net/browse/DESC-6039)`
+    owner,
+    repo,
+    pull_number: issue_number,
+    body: `[**DESC-1234**](https://pepsico-ecomm.atlassian.net/browse/DESC-6039)\n` + context.payload.pull_request.body
   })
 
-  console.log(context.payload.pull_request.number)
-  console.log(context.issue.number)
+  await github.rest.issues.addAssignees({
+    owner,
+    repo,
+    issue_number,
+    assignees: [creator]
+  })
+
 
 
   await github.rest.issues.createComment({
-    issue_number: context.issue.number,
-    owner: context.repo.owner,
-    repo: context.repo.repo,
+    issue_number,
+    owner,
+    repo,
     body: `title: ${context.payload.pull_request.title} by ${creator}`
   })
 }
