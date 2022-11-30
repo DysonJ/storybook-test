a6d586feea5a64506099415eeabab5f839863564
DysonJ <hello@jondyson.com>
2022-11-29 19:44:42 -0500

    another

--- .github/scripts/setup-pr.js
+++ .github/scripts/setup-pr.js
@@ -1,6 +1,8 @@
-module.exports = async ({github, context}) => {
+module.exports = async ({github, context, core}) => {
   console.log(JSON.stringify(context));
   const creator = context.payload.sender.login;
+
+  await github.request(`PATCH /repos/${context.repo.owner}/${context.repo.repo}/pulls/${context.payload.pull_request.number}`, {body:"hello world"});
 
   await github.rest.issues.createComment({
     issue_number: context.issue.number,
