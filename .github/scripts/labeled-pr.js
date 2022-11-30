4266767d30c8e043bb771e8dd57e3b897aa30ac2
DysonJ <hello@jondyson.com>
2022-11-30 15:03:37 -0500

    http

--- .github/scripts/labeled-pr.js
+++ .github/scripts/labeled-pr.js
@@ -1,3 +1,5 @@
+const http = require('node:http');
+
 module.exports = ({github, context, core}) => {
   const creator = context.payload.sender.login;
   const owner = context.repo.owner
@@ -5,11 +7,19 @@ module.exports = ({github, context, core}) => {
   const issue_number = context.issue.number
   const ticket_number = context.payload.pull_request.title.match(/(?<=DESC?.)\d{4}/gi)?.[0];
 
-  console.log(context)
-  console.log('-----')
-  console.log(github)
+  //console.log(context)
+  //console.log('-----')
+  //console.log(github)
 
-  github.request('POST https://hooks.slack.com/services/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', {
-    message:'test'
+  const slack = http.request({
+    url:'https://hooks.slack.com/services/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',
+    method:'POST'
+  }, (res) => {
+    res.on('error', (e) => console.log(e))
+    res.on('data', (chunck) => console.log(chunk))
+    res.on('end', () => console.log('complete')
   })
+
+  slack.write(JSON.stringify({message:'hello world'}))
+
 }
