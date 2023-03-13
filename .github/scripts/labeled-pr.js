b41e32f17fda9c31ebe37f209bb3e7b2a7982893
DysonJ <hello@jondyson.com>
2023-03-13 18:31:04 -0400

    update script

--- .github/scripts/labeled-pr.js
+++ .github/scripts/labeled-pr.js
@@ -14,59 +14,59 @@ module.exports = ({github, context}) => {
     pull_number: issue_number,
     reviewers:['happy']
   })
- //test
-  // const header = {
-  //   type: "section",
-  //   text: {
-  //     type: "mrkdwn",
-  //     text: "*Ready for Review* :tada: \n DESC-1234 This is a description of the thing"
-  //   }
-  // }
+  
+  const header = {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: "*Ready for Review* :tada: \n DESC-1234 This is a description of the thing"
+    }
+  }
 
 
-  // const details = {
-  //   type: "section",
-  //   fields: [
-  //     {
-  //       type: "mrkdwn",
-  //       text: `*PR:* <https://github.com/pepsico-ecommerce/ecomm-engine-frontend/pull/${issue_number}|${issue_number}>`
-  //     }
-  //   ]
-  // }
+  const details = {
+    type: "section",
+    fields: [
+      {
+        type: "mrkdwn",
+        text: `*PR:* <https://github.com/pepsico-ecommerce/ecomm-engine-frontend/pull/${issue_number}|${issue_number}>`
+      }
+    ]
+  }
 
-  // if(ticket_number){
-  //   details.fields.push({
-  //     type: "mrkdwn",
-  //     text: `*Jira:* <https://pepsico-ecomm.atlassian.net/browse/DESC-${ticket_number}|DESC-${ticket_number}>`
-  //   })
-  // }
+  if(ticket_number){
+    details.fields.push({
+      type: "mrkdwn",
+      text: `*Jira:* <https://pepsico-ecomm.atlassian.net/browse/DESC-${ticket_number}|DESC-${ticket_number}>`
+    })
+  }
 
-  // const author = {
-  //   type: "context",
-  //   elements: [
-  //     {
-  //       type: "mrkdwn",
-  //       text: `*Author*: ${creator}`
-  //     }
-  //   ]
-  // }
+  const author = {
+    type: "context",
+    elements: [
+      {
+        type: "mrkdwn",
+        text: `*Author*: ${creator}`
+      }
+    ]
+  }
 
-  // let body = ''
-  // const slack = https.request({
-  //   protocol: 'https:',
-  //   hostname:'hooks.slack.com',
-  //   port: 443,
-  //   method:'POST',
-  //   path:`/services/${process.env.SLACK_WEBHOOK_PATH}`,
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   }
-  // }, (res) => {
-  //   res.on('error', (e) => console.log(e))
-  //   res.on('data', (chunk) => body += chunk.toString('utf8'))
-  //   res.on('end', () => console.log(body))
-  // })
+  let body = ''
+  const slack = https.request({
+    protocol: 'https:',
+    hostname:'hooks.slack.com',
+    port: 443,
+    method:'POST',
+    path:`/services/${process.env.SLACK_WEBHOOK_PATH}`,
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }, (res) => {
+    res.on('error', (e) => console.log(e))
+    res.on('data', (chunk) => body += chunk.toString('utf8'))
+    res.on('end', () => console.log(body))
+  })
 
-  // slack.write(JSON.stringify({ "blocks": [header, details, author]}))
-  // slack.end()
+  slack.write(JSON.stringify({ "blocks": [header, details, author]}))
+  slack.end()
 }
